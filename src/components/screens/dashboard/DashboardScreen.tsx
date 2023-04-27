b5a856940f2ad6d6f4afc7a34aa27f3f8ed2b365
jhonson-lc <jhoncisneros11@gmail.com>
2023-04-27 00:53:42 -0500

    Change dashboard screen

--- src/components/screens/dashboard/DashboardScreen.tsx
+++ src/components/screens/dashboard/DashboardScreen.tsx
@@ -1,26 +1,9 @@
-import { EmptyStateWrapper } from "../../common/EmptyStateWrapper";
 import { MainHeading } from "../../common/MainHeading";
 
-import { EmptyStateDashboard } from "./EmptyStateDashboard";
-import { EnrolledList } from "./EnrolledList";
-
-import { api } from "@/utils/api";
-
 export const DashboardScreen = () => {
-  const enrolledClassroomsQuery = api.Student.getClassrooms.useQuery();
-
-  const { data: classrooms, isLoading } = enrolledClassroomsQuery;
-
   return (
     <div>
       <MainHeading title="Your Classrooms" />
-
-      <EmptyStateWrapper
-        EmptyComponent={<EmptyStateDashboard />}
-        NonEmptyComponent={<EnrolledList classrooms={classrooms ?? []} />}
-        data={classrooms}
-        isLoading={isLoading}
-      />
     </div>
   );
 };
