3586673d9decfc90e6d116e564e8188cbc681a8b
jhonson-lc <jhoncisneros11@gmail.com>
2023-04-27 00:51:38 -0500

    New style

--- src/ui/controls/Switch.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Switch as ChakraSwitch, SwitchProps } from "@chakra-ui/react";
-
-const Switch: React.FC<SwitchProps> = (props) => {
-  return <ChakraSwitch {...props} />;
-};
-
-export default Switch;
