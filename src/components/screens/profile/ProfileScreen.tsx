00dca9b9d654d2823d9be0da5036a6bfb9f12bc3
jhonson-lc <jhoncisneros11@gmail.com>
2023-04-27 00:00:22 -0500

    Add page profile

--- src/components/screens/profile/ProfileScreen.tsx
+++ src/components/screens/profile/ProfileScreen.tsx
@@ -1,11 +1,12 @@
-import { User } from '@prisma/client';
-import { useForm } from 'react-hook-form';
-import { HeaderLayout } from '../../../layouts/HeaderLayout';
-import { trpc } from '../../../utils/trpc';
-import { Alert, useDismissible } from '../../common/Alert';
-import { Button } from '../../common/Button/Button';
-import { FormGroup } from '../../common/Form/FormGroup/FormGroup';
-import { MainHeading } from '../../common/MainHeading';
+import { User } from "@prisma/client";
+import { useForm } from "react-hook-form";
+
+import { Alert, useDismissible } from "../../common/Alert";
+import { Button } from "../../common/Button/Button";
+import { MainHeading } from "../../common/MainHeading";
+
+import { api } from "@/utils/api";
+import { FormGroup } from "@/ui/form/FormGroup";
 
 export const ProfileScreen = () => {
   const { dismiss, show, isDisplayed } = useDismissible();
@@ -17,61 +18,52 @@ export const ProfileScreen = () => {
     setValue,
   } = useForm();
 
-  const updateDisplayName = trpc.user.updateDisplayName.useMutation();
+  const updateDisplayName = api.User.updateDisplayName.useMutation();
 
-  trpc.user.getUser.useQuery(undefined, {
+  api.User.getUser.useQuery(undefined, {
     onSuccess(userData: User) {
-      setValue('displayName', userData.displayName ?? userData?.name);
+      setValue("displayName", userData.displayName ?? userData?.name);
     },
   });
 
-  const queryClient = trpc.useContext();
+  const ctx = api.useContext();
 
-  const handleProfileSubmit = async (data) => {
+  const handleProfileSubmit = async (data: any) => {
     await updateDisplayName.mutateAsync({
       displayName: data.displayName,
     });
-    queryClient.user.getUser.invalidate();
+    ctx.User.getUser.invalidate();
     show();
   };
 
   return (
-    <HeaderLayout>
-      <MainHeading title="Your Profile" />
+    <>
+      <MainHeading title="Tu Perfil" />
 
       {isDisplayed && (
-        <Alert
-          message="Your profile has been sucessfully updated."
-          onClose={dismiss}
-        />
+        <Alert message="Your profile has been sucessfully updated." onClose={dismiss} />
       )}
 
-      <h2 className="text-2xl mb-4">Settings</h2>
+      <h2 className="mb-4 text-2xl">
+        <span className="font-bold">Información personal</span>
+      </h2>
 
-      <form
-        onSubmit={handleSubmit(handleProfileSubmit)}
-        className="w-1/3"
-      >
+      <form className="w-1/3" onSubmit={handleSubmit(handleProfileSubmit)}>
         <FormGroup
-          label="Display Name"
-          error={errors.displayName && 'Display name is required'}
+          error={errors.displayName && "Display name is required"}
+          label="Nombre de usuario"
           name="displayName"
         >
-          <>
-            <input
-              id="displayName"
-              className="mb-2"
-              {...register('displayName', { required: true })}
-            />
-            <Button
-              isLoading={updateDisplayName.isLoading}
-              className="self-end"
-            >
-              Update
-            </Button>
-          </>
+          <input
+            className="focus:ring-primary-500 mb-2 rounded-md bg-slate-100 p-2 focus:border-transparent focus:outline-none focus:ring-2"
+            id="displayName"
+            {...register("displayName", { required: true })}
+          />
         </FormGroup>
+        <Button className="self-end" isLoading={updateDisplayName.isLoading}>
+          Actualizar
+        </Button>
       </form>
-    </HeaderLayout>
+    </>
   );
 };
