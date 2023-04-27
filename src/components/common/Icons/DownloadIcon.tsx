6e07986a64f9b3f5491b495d9f6beef80fad669b
jhonson-lc <jhoncisneros11@gmail.com>
2023-04-27 00:51:08 -0500

    Add icons

---
+++ src/components/common/Icons/DownloadIcon.tsx
@@ -0,0 +1,20 @@
+import { withSize } from "./Icon";
+
+export const DownloadIcon = withSize(({ sizeClass }) => {
+  return (
+    <svg
+      className={sizeClass}
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+});
