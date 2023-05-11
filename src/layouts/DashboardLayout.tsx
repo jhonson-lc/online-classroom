280e3a701350372df14f79a84f91410c91e11b8a
jhonson-lc <jhoncisneros11@gmail.com>
2023-05-10 23:43:41 -0500

    Add dashboard layout

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Header } from "@/components/common/Header/Header";
 import Sidebar from "@/components/common/Sidebar/Sidebar";
 import { useSession } from "@/libs/useSession";
+import SidebarLoadingScreen from "@/components/common/Sidebar/SidebarLoadingScreen";
 
 interface Props {
   children: React.ReactNode;
@@ -10,12 +11,26 @@ interface Props {
 
 const DashboardLayout: React.FC<Props> = ({ children }) => {
   const session = useSession();
-  const userRole = session.data?.user.role;
+  const userRole = session?.data?.user?.role ?? "student";
+
+  if (!userRole) {
+    return (
+      <main className="flex w-full">
+        <section>
+          <SidebarLoadingScreen />
+        </section>
+        <section className="flex w-full flex-col">
+          <Header />
+          <div className="h-full w-full p-6">{children}</div>
+        </section>
+      </main>
+    );
+  }
 
   return (
     <>
       <main className="flex w-full">
-        <section className="w-full max-w-[17%]">
+        <section>
           {userRole === "teacher" ? <Sidebar role="teacher" /> : <Sidebar role="student" />}
         </section>
         <section className="flex w-full flex-col">
