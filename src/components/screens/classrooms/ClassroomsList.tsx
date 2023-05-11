4dee56e7d96adee75d62e885ecbc125435cf6c4c
jhonson-lc <jhoncisneros11@gmail.com>
2023-05-10 23:43:19 -0500

    Add list of classroom

---
+++ src/components/screens/classrooms/ClassroomsList.tsx
@@ -0,0 +1,16 @@
+import { Classroom } from "@prisma/client";
+import React from "react";
+
+import { ClassroomCard } from "./ClassroomCard";
+
+export const ClassroomsList = ({ classrooms }: { classrooms: Classroom[] }) => {
+  return (
+    <div className="flex flex-col gap-4">
+      <ul className="grid grid-cols-3 gap-4">
+        {classrooms.map((classroom) => (
+          <ClassroomCard key={classroom.id} classroom={classroom} />
+        ))}
+      </ul>
+    </div>
+  );
+};
