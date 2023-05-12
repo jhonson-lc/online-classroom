79832a07d9053342e2d213f46f529843eead6a9f
jhonson-lc <jhoncisneros11@gmail.com>
2023-05-11 23:50:46 -0500

    Add new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,13 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3A1078",
+        secondary: "#2F58CD",
+        white: "#ffffff",
+      },
+    },
     transitionProperty: {
       width: "width",
     },
