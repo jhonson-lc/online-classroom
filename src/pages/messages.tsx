74d514d109b7d67e66472650b40728c0077169a0
jhonson-lc <jhoncisneros11@gmail.com>
2023-05-24 23:57:06 -0500

    Add page message

--- src/pages/messages.tsx
+++ src/pages/messages.tsx
@@ -2,14 +2,37 @@ import { NextPage } from "next";
 
 import { MainHeading } from "@/components/common/MainHeading";
 import DashboardLayout from "@/layouts/DashboardLayout";
+import MessageScreen from "@/components/screens/message/MessageScreen";
+import { UnstableGetServerSession } from "@/libs/UnstableGetServerSession";
+import { useSession } from "@/libs/useSession";
 
 const Messages: NextPage = () => {
+  const session = useSession();
+  const userData = session.data?.user;
+
+  if (!userData) return null;
+
   return (
     <DashboardLayout>
-      <MainHeading title="Mensajes" />
-      <span>Lista de mensajes</span>
+      <MainHeading title="Mensajería" />
+      <MessageScreen role={userData.role} />
     </DashboardLayout>
   );
 };
+
+export async function getServerSideProps(context: any) {
+  const session = await UnstableGetServerSession(context);
+
+  if (!session?.user) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  } else {
+    return { props: {} };
+  }
+}
 
 export default Messages;
